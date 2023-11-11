7c8578a8c15e1597b43cddefb651cdcd5266ba82
JURI <vwnflv74@naver.com>
2023-11-11 18:15:50 +0900

    초기설정

---
+++ src/main/java/com/surfholicdb/surfholicdb/SurfholicdbApplication.java
@@ -0,0 +1,13 @@
+package com.surfholicdb.surfholicdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfholicdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfholicdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surfholicdb/surfholicdb/SurfholicdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfholicdb.surfholicdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfholicdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
